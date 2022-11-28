699767950d40673541acaecff1c5f4325b391f80
threadi <tzwirne@gmx.de>
2022-11-28 12:08:05 +0100

    fix group title template embedding

--- templates/parts/part-grouptitle.php
+++ templates/parts/part-grouptitle.php
@@ -11,7 +11,7 @@ if( !empty($personio_attributes['groupby']) ) {
     // get the title of the given grouped taxonomy of this position
     $newGroupTitle = helper::get_taxonomy_name_of_position($personio_attributes['groupby'], $position);
 
-    // output title if if has been changed during the loop
+    // output title if it has been changed during the loop
     if (strcmp($newGroupTitle, $groupTitle)) {
         $groupTitle = $newGroupTitle;
         echo '<h2>' . $newGroupTitle . '</h2>';
