1b1f47a0d66a0db011ef3573847f681476463afc
threadi <tzwirne@gmx.de>
2023-06-22 16:24:03 +0200

    add anchor for filter links

--- templates/parts/term-filter-linklist.php
+++ templates/parts/term-filter-linklist.php
@@ -15,6 +15,9 @@ defined( 'ABSPATH' ) || exit;
             for( $t=0;$t<count($terms);$t++ ) {
                 if( !empty($terms[$t]) ) {
                     $url = add_query_arg('personiofilter['.$filter.']', $terms[$t]->term_id);
+                    if( !empty($form_id) ) {
+                        $url .= '#'.$form_id;
+                    }
                     ?><li><a href="<?php echo esc_url($url); ?>"<?php echo ($terms[$t]->term_id == $value ? ' class="personio-filter-selected"' : ''); ?>><?php echo esc_html($terms[$t]->name); ?></a></li><?php
                 }
             }
