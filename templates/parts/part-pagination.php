1e61163888d84b714e6c9245d19f6ddcc6fb6153
threadi <tzwirne@gmx.de>
2023-07-06 14:02:23 +0200

    add anchor to pagination

--- templates/parts/part-pagination.php
+++ templates/parts/part-pagination.php
@@ -5,19 +5,23 @@ defined( 'ABSPATH' ) || exit;
 /**
  * Output of pagination.
  *
- * @version: 1.0.0
+ * @version: 1.1.0
  */
 ?>
-    <div class="entry-content">
-        <p>
-            <?php
-                $query = [
-                    'base' => str_replace( PHP_INT_MAX, '%#%', esc_url( get_pagenum_link( PHP_INT_MAX ) ) ),
-                    'format' => '?paged=%#%',
-                    'current' => max( 1, get_query_var('paged') ),
-                    'total' => $positionsObj->getResult()->max_num_pages
-                ];
-                echo paginate_links($query);
-            ?>
-        </p>
-    </div>
+<div class="entry-content">
+    <p>
+        <?php
+            $url = '';
+            if( !empty($form_id) ) {
+                $url .= '#'.$form_id;
+            }
+            $query = [
+                'base' => str_replace( PHP_INT_MAX, '%#%', esc_url( get_pagenum_link( PHP_INT_MAX ) ) ).$url,
+                'format' => '?paged=%#%',
+                'current' => max( 1, get_query_var('paged') ),
+                'total' => $positionsObj->getResult()->max_num_pages
+            ];
+            echo paginate_links($query);
+        ?>
+    </p>
+</div>
