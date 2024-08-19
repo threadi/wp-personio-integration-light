1006d1184c126ee8723972dadfe66da459ca5be4
threadi <tzwirne@gmx.de>
2024-08-19 09:50:21 +0200

    limit log deletion depending on db type

--- app/Log.php
+++ app/Log.php
@@ -93,7 +93,7 @@ class Log {
 		global $wpdb;
 
 		// run with limit if it is not sqlite.
-		if( str_contains( strtolower( $wpdb->db_server_info() ), 'sqlite' ) ) {
+		if ( str_contains( strtolower( $wpdb->db_server_info() ), 'sqlite' ) ) {
 			$wpdb->query( sprintf( 'DELETE FROM %s WHERE `time` < DATE_SUB(NOW(), INTERVAL %d DAY)', esc_sql( $wpdb->prefix . 'personio_import_logs' ), absint( get_option( 'personioIntegrationMaxAgeLogEntries' ) ) ) );
 			return;
 		}
