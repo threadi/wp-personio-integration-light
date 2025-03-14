58243b80192e7f19f760a6f7881ed5f52e139f93
threadi <tzwirne@gmx.de>
2025-03-14 15:38:51 +0100

    optimized pagination of log table

--- app/Plugin/Admin/Logs.php
+++ app/Plugin/Admin/Logs.php
@@ -11,6 +11,7 @@ namespace PersonioIntegrationLight\Plugin\Admin;
 defined( 'ABSPATH' ) || exit;
 
 use PersonioIntegrationLight\Log_Table;
+use PersonioIntegrationLight\PersonioIntegration\PostTypes\PersonioPosition;
 
 /**
  * Object to handle the output.
@@ -23,22 +24,37 @@ class Logs {
 	 * @return void
 	 */
 	public static function show(): void {
-		if ( current_user_can( 'manage_options' ) ) {
-			// if WP_List_Table is not loaded automatically, we need to load it.
-			if ( ! class_exists( 'WP_List_Table' ) ) {
-				require_once ABSPATH . 'wp-admin/includes/class-wp-list-table.php';
-			}
-			$log = new Log_Table();
-			$log->prepare_items();
-			?>
-			<div class="wrap">
-				<h2><?php echo esc_html__( 'Logs', 'personio-integration-light' ); ?></h2>
+		// bail if user has not the capability for this.
+		if ( ! current_user_can( 'manage_options' ) ) {
+			return;
+		}
+
+		// get page from request.
+		$page = filter_input( INPUT_GET, 'page', FILTER_SANITIZE_FULL_SPECIAL_CHARS );
+
+		// if WP_List_Table is not loaded automatically, we need to load it.
+		if ( ! class_exists( 'WP_List_Table' ) ) {
+			require_once ABSPATH . 'wp-admin/includes/class-wp-list-table.php';
+		}
+
+		// get the table object.
+		$log = new Log_Table();
+		$log->prepare_items();
+
+		// output.
+		?>
+		<div class="wrap">
+			<h2><?php echo esc_html__( 'Logs', 'personio-integration-light' ); ?></h2>
+			<form action="<?php echo esc_url( get_admin_url() . 'edit.php' ); ?>" method="get">
+				<input type="hidden" name="page" value="<?php echo esc_attr( $page ); ?>">
+				<input type="hidden" name="tab" value="logs">
+				<input type="hidden" name="post_type" value="<?php echo esc_attr( PersonioPosition::get_instance()->get_name() ); ?>">
 				<?php
 				$log->views();
 				$log->display();
 				?>
-			</div>
-			<?php
-		}
+			</form>
+		</div>
+		<?php
 	}
 }
--- changelog.md
+++ changelog.md
@@ -8,6 +8,7 @@
 
 ### Optimized
 
+- Pagination of log table is now optimized
 - composer.json is now part of the release
 
 ## [4.2.4] - 10.03.2025
