50e110fde52ad8dea62a8f28b638ae1348e9ac18
threadi <tzwirne@gmx.de>
2025-04-23 10:18:55 +0200

    changed usage of custom styles for widgets in classic theme

---
+++ legacy/styles.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * File which output CSS from classic themes.
+ *
+ * @param array $attributes List of attributes for the used widget.
+ *
+ * @package personio-integration-light
+ */
+
+echo '<style>' . $attributes['styles'] . '</style>';
